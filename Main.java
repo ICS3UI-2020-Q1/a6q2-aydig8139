ac98b8696b21f78508423cc94322699890a1e7bb
aydig8139 <aydig8139@wrdsb.ca>
2020-10-08 17:46:33 +0000

    finished search algorithm question

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * search algorithm
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new scanner fo user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //create an array to store the integers
+    int[] integers = new int[10];
+
+    //use a for loop to get all the integers from the user
+    for(int i = 0; i < integers.length; i++){
+      integers[i] = input.nextInt();
+    }
+
+    //ask user for an integer to find and declare a variable to store that number
+    System.out.println("Please enter a number to find");
+    int find = input.nextInt();
+
+    //create a for loop to determine the index that the located number is on
+    for(int i = 0; i < integers.length; i++){
+      
+      //create an if statement to determine if an integer in the set is equal to the number asked for
+      if (integers[i] == find){
+        System.out.println(find + " is located at index " + (i+1));
+      }else{
+        System.out.println("The number you have asked for is not in the set");
+      }
+    }
     
   }
 }
